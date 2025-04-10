193d145607955caf36653ffde365fce9e96bbfc0
Hunter104 <000plagueinc@gmail.com>
2025-04-10 20:59:33 -0300

    Adiciona leitura de nível

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <cerrno>
+#include <istream>
 #include <list>
+#include <optional>
 #include <unistd.h>
 #include <cstdlib>
 #include <cstring>
@@ -61,13 +63,17 @@ struct Vector2 {
   friend std::ostream &operator<<(std::ostream &os, const Vector2 &v) {
     return os << "(" << v.x << ", " << v.y << ")";
   }
+
+  friend std::istream &operator>>(std::istream& in, Vector2& v) {
+    return in >> v.x >> v.y;
+  }
 };
 
 class KnowledgeBase {
 private:
   std::vector<std::vector<int>> hasBombVariables;
   std::map<int, Vector2> inverseLookupMap;
-  std::list<std::list<int>> clauses;
+  std::vector<std::vector<int>> clauses;
 
 public:
   KnowledgeBase(int width, int height) {
@@ -87,10 +93,72 @@ public:
   }
 };
 
+class Level {
+private:
+  int size;
+  std::optional<int> bombCount;
+  std::vector<std::vector<int>> playingField;
+public:
+  Level(int size, int bombCount)
+    : size(size),
+      bombCount(bombCount >= 0 ? std::optional<int>{bombCount} : std::nullopt),
+      playingField(size, std::vector<int>(size, -1))
+  {}
+
+  void setCell(Vector2 pos, int value) {
+    playingField[pos.x][pos.y] = value;
+  }
+
+  void updateFromStream(std::istream& in) {
+    int openCellsCount;
+    in >> openCellsCount;
+    for (int i=0; i < openCellsCount; i++) {
+      Vector2 pos; 
+      int num;
+      in >> pos >> num;
+      setCell(pos, num);
+    }
+  }
+
+  static Level createFromStream(std::istream& in) {
+    int size, bombs, openCellsCount;
+    in >> size >> bombs >> openCellsCount;
+
+    Level level(size, bombs);
+    for (int i=0; i < openCellsCount; i++) {
+      Vector2 pos; 
+      int num;
+      in >> pos >> num;
+      level.setCell(pos, num);
+    }
+
+    return level;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const Level& level) {
+    for (int i=0; i<level.size; i++) {
+      for (int j=0; j<level.size; j++) {
+        int tile = level.playingField[i][j];
+        if (tile == -1)
+          os << "# ";
+        else if (tile == 0)
+          os << ". ";
+        else 
+          os << tile << ' ';
+      }
+      os << '\n';
+    }
+    return os;
+  }
+  
+};
+
 int main(void) {
   if (system("which clasp > /dev/null 2>&1")) {
     std::cerr << "Could not find clasp command, are you sure it is installed?\n";
     return 1;
   }
-  return 0;
+
+  Level level = Level::createFromStream(std::cin);
+  std::cout << level;
 }
