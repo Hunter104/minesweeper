fcbc94e94158d2a805a19834528e200f037dfed3
Hunter104 <000plagueinc@gmail.com>
2025-04-10 12:52:07 -0300

    Refatoração para maior estabilidade

--- src/main.cpp
+++ src/main.cpp
@@ -1,62 +1,61 @@
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
-
 struct Vector2 {
-  int x, y;
+  int x = 0, y = 0;
 
   Vector2() = default;
   Vector2(int x, int y) : x(x), y(y) {}
-  Vector2(Vector2& other) : x(other.x), y(other.y) {}
+  Vector2(const Vector2 &other) = default;
 
-  Vector2 operator+(const Vector2& other) {
-    return Vector2(x+other.x, y+other.y);
+  Vector2 operator+(const Vector2 &other) const {
+    return Vector2(x + other.x, y + other.y);
   }
 
-  Vector2 operator-(const Vector2& other) {
-    return Vector2(x-other.x, y-other.y);
+  Vector2 operator-(const Vector2 &other) const {
+    return Vector2(x - other.x, y - other.y);
   }
 
-  Vector2 operator*(const int other) {
-    return Vector2(x*other, y*other);
+  Vector2 operator*(int scalar) const {
+    return Vector2(x * scalar, y * scalar);
   }
 
-  void operator+=(const Vector2& other) {
+  Vector2 &operator+=(const Vector2 &other) {
     x += other.x;
     y += other.y;
+    return *this;
   }
 
-  void operator-=(const Vector2& other) {
+  Vector2 &operator-=(const Vector2 &other) {
     x -= other.x;
     y -= other.y;
+    return *this;
   }
 
-  void operator *=(const int other) {
-    x *= other;
-    y *= other;
+  Vector2 &operator*=(int scalar) {
+    x *= scalar;
+    return *this;
   }
 
-  bool operator==(const Vector2& other) const {
-      return x == other.x && y == other.y;
+  bool operator==(const Vector2 &other) const {
+    return x == other.x && y == other.y;
   }
 
-  bool operator!=(const Vector2& other) const {
-      return !(*this == other);
+  bool operator!=(const Vector2 &other) const { return !(*this == other); }
+
+  static const vector<Vector2> &AllDirections() {
+    static const vector<Vector2> directions = {
+        {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    return directions;
   }
 
-
-  static const vector<Vector2>& AllDirections() {
-      static const vector<Vector2> directions = {
-          {-1, -1}, {-1, 0}, {-1, 1},
-          { 0, -1},          { 0, 1},
-          { 1, -1}, { 1, 0}, { 1, 1}
-      };
-      return directions;
+  friend ostream &operator<<(ostream &os, const Vector2 &v) {
+    return os << "(" << v.x << ", " << v.y << ")";
   }
 };
-
-int main (int argc, char *argv[]) {
+int main(int argc, char *argv[]) {
   std::cout << "Hello";
   return 0;
 }
