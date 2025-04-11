b6a5e13aa8d367732f00b2dde635ff201cb8d9f6
Hunter104 <000plagueinc@gmail.com>
2025-04-10 22:13:29 -0300

    Adiciona implementação inicial da KB

---
+++ src/knowledgebase.cpp
@@ -0,0 +1,58 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+#include <unistd.h>
+#include <vector>
+#include "vector2.cpp"
+#pragma once
+
+class KnowledgeBase {
+private:
+  std::vector<std::vector<int>> hasBombVariables;
+  std::map<int, Vector2> inverseLookup;
+  std::vector<std::vector<int>> clauses;
+  int variableCount = 0;
+public:
+  KnowledgeBase(int mapSize) :
+  hasBombVariables(mapSize, std::vector<int>(mapSize, -1)){
+    for (int x=0; x<mapSize; x++) {
+      for (int y=0; y<mapSize; y++) {
+        hasBombVariables[x][y] = variableCount+1;
+        inverseLookup[variableCount+1] =  {x, y};
+        variableCount++;
+      }
+    }
+  }
+
+  bool query() {
+    char filename[] = "/tmp/minesweeper.XXXXXX";
+    int fd = mkstemp(filename);
+    if (fd < 0)
+      throw std::runtime_error("Could not create tempfile.");
+    std::ostringstream text;
+    text << "p cnf " << variableCount << clauses.size() << '\n';
+    for (auto& clause : clauses) {
+      for (auto& variable : clause) {
+        text << variable << ' ';
+      }
+      text << "0\n";
+    }
+
+    std::string result = text.str();
+    const char *cText = result.c_str();
+    write(fd, cText, strlen(cText));
+    close(fd);
+
+    std::string command = "clasp ";
+    command += filename;
+    command += " > /dev/null";
+
+    int rc = std::system(command.c_str());
+
+    unlink(filename);
+    return rc == 20;
+  }
+};
