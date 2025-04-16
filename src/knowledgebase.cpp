6846104a5aa5812435b3f90a9bd22a43f7d33af5
Hunter104 <000plagueinc@gmail.com>
2025-04-16 13:00:45 -0300

    Adciona claúsula para espaços vazios

--- src/knowledgebase.cpp
+++ src/knowledgebase.cpp
@@ -105,7 +105,7 @@ public:
   void feedNewInfo(ILevel* level) {
     const std::vector<std::pair<Vector2, int>> &openCells = level->getOpenCells();
     for (auto& cell : openCells) {
-      if (cell.second == 0) continue;
+      solver.addClause({-hasBombVariables[cell.first]});
       std::vector<int> variables;
       for (auto& adjacent : level->getUnkownAdjacent(cell.first)) {
         variables.push_back(hasBombVariables[adjacent]);
